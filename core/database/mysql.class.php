ad4bd3224220d51967ef1acbdb58dbae84fb8525
CrystalEngine <fusion@razorirc.info>
2009-03-24 23:47:53 -0400

    fixed a little error on my part
    Signed-off-by: CrystalEngine <fusion@razorirc.info>

---
+++ core/database/mysql.class.php
@@ -0,0 +1,10 @@
+<?php
+    /*
+      Package Name: Crystal Engine
+      File Name: mysql.class.php (mysql database driver)
+      Version: 0.0.1
+      Author: Matthew Sporich <fusion@razorirc.com>
+      License: MIT License
+    */
+
+?>
---
+++ core/database/mysqli.class.php
@@ -0,0 +1,10 @@
+<?php
+    /*
+      Package Name: Crystal Engine
+      File Name: mysqli.class.php (mysqli database driver)
+      Version: 0.0.1
+      Author: Matthew Sporich <fusion@razorirc.com>
+      License: MIT License
+    */
+
+?>
